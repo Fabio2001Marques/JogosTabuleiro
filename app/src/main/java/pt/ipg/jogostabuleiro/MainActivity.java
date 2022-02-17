589284122cc0159ce37678c44649ee444cc8c3bc
Fabio Marques <fabio2001marques@gmail.com>
2022-02-17 18:02:15 +0000

    Layout Jogo do Galo Completo

--- app/src/main/java/pt/ipg/jogostabuleiro/MainActivity.java
+++ app/src/main/java/pt/ipg/jogostabuleiro/MainActivity.java
@@ -3,12 +3,17 @@ package pt.ipg.jogostabuleiro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.jogo_galo);
+    }
+
+    public void tab1_1(View v) {
+            System.out.print("xxx");
     }
 }
